d7ef65b56007c58c7d16ada18e962750083d0eeb
tanakah <fivestarlks@gmail.com>
2021-02-12 00:13:56 +0200

    Answer to Task 0.2

---
+++ Task0,2.js
@@ -0,0 +1,16 @@
+var x,y,z,a,b;
+function demoBodmas(){
+    x = 1 + 1 * 2;
+    y = (1 + 1) * 2;
+    z = 1 + ( 1 * 2 );
+    a = 1 + 1 * 2 / 2;
+    b = (1 + 1 * 2 ) / 2; 
+
+    console.log(x+y+z+a+b);
+    console.log(y);
+    console.log(z);
+    console.log(a);
+    console.log(b);
+}
+    demoBodmas();
+
