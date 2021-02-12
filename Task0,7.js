42e188716466b03fb2ff7f7ce7c7d6c99c74e4d0
tanakah <fivestarlks@gmail.com>
2021-02-12 03:44:44 +0200

    Answer to Task 0.7

--- Task0,7.js
+++ Task0,7.js
@@ -1,8 +1,10 @@
+//Function that changes celsius to fahreinheit degrees.
 function celsiusToFahreinheit(celsiusDegrees){
     return fahreinheitDegrees = celsiusDegrees * 1.8 + 32;
 }
 celsiusToFahreinheit(10);
 
+//Function that changes fahreinheit to celsius degress.
 function fahreinheitToCelsius(fahreinheitDegrees){
     return celciusDegrees = (fahreinheitDegrees - 32) * (5/9);
 }
