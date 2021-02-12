512402a78676f479c5611491db88ebd86565ef9e
tanakah <fivestarlks@gmail.com>
2021-02-12 03:42:08 +0200

    Answer to Task 0.7

---
+++ Task0,7.js
@@ -0,0 +1,10 @@
+function celsiusToFahreinheit(celsiusDegrees){
+    return fahreinheitDegrees = celsiusDegrees * 1.8 + 32;
+}
+celsiusToFahreinheit(10);
+
+function fahreinheitToCelsius(fahreinheitDegrees){
+    return celciusDegrees = (fahreinheitDegrees - 32) * (5/9);
+}
+
+fahreinheitToCelsius(50);
