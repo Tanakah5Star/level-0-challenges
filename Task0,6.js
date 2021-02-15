b152706530427cd6c44638ebb4bb47499922ed48
tanakah <fivestarlks@gmail.com>
2021-02-15 14:02:30 +0200

    Answer to Task0,6

--- Task0,6.js
+++ Task0,6.js
@@ -1,11 +1,12 @@
 //Making use of the if..else-if statement to equate numbers and come up with the largest.
-function checkMaximumNumber(number1,number2,number3){
-if ( number1 > number2 && number1 > number3){
-    return "The Largest number is "+ number1;
-}else if (number2 > number3){
-    return "The Largest number is "+ number2;
+function checkMaximumNumber(number1, number2, number3){
+
+   if ( number1 > number2 && number1 > number3){
+         return "The Largest number is "+ number1;
+      }else if (number2 > number3){
+         return "The Largest number is "+ number2;
 }
-    return "The Largest number is "+ number3;
+         return "The Largest number is "+ number3;
 
 }
-checkMaximumNumber(78,2,9);
+console.log(checkMaximumNumber(78,2,9));
