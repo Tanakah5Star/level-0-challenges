30cd29f3ba88382f1efaeefc7afcdb9571ed320f
tanakah <fivestarlks@gmail.com>
2021-02-15 13:55:18 +0200

    Answer to Task0,10

--- Task0,10.js
+++ Task0,10.js
@@ -1,15 +1,42 @@
-//function findCommonLetters(firstString, secondString) {  
-//}
-function countCommonCharacters(userWord, goalWord){
-    var compareNum = 0;    
-    var check = Math.min(userWord.length, goalWord.length);
-    for (var i = 0; i < check; i++) {
-      if (userWord.charAt(i) == goalWord.charAt(i))
-      compareNum++;
-    }
-    return compareNum;
+//function findCommonCharacters(stringOne, stringTwo) {
+ // var stringOne = stringOne.split("");
+  //var stringTwo = stringTwo.split("");
+
+  //function returnSubsequence(string1,string2){
+
+    //var string1; //= stringOne.split("");
+    //var  string2; //= stringTwo.split("");
+
+    /*let resultString = ' ';
+    for (let i of string1){
+        for(let n of string2){
+            if( n === i){
+               resultString.push(n);
+            }
+        }
+    };
+    return resultString;
   }
-  var goalWord = "12345678";
-  var userWord = "02345000";
-  var result = countCommonCharacters(userWord, goalWord);
-  showResult(result);
+         
+console.log(returnSubsequence("tanakah","mashoko"));*/
+
+
+function findCommonCharacters(string1, string2) {
+  string1 += "";
+  string2 += "";
+  //function check for common characters in both strings comparing each character.
+  var i, a ={};
+  for (i = 0, l = string1.length; i < l; i++) a[string1[i]] = (a[string1[i]] || 0) + 1;
+  for (i = 0, l = string2.length; i < l; i++) a[string2[i]] = (a[string2[i]] || 0) + 1;
+  // function deletes all the characters that are not common to both strings.
+  for (var p in a)
+  if (a[p] <= 1)
+  delete a[p];
+  
+  return a;
+  
+  }
+  var output = findCommonCharacters('bullion', 'albion');
+    for (var a in output) {
+      console.log("Output: " + a);
+  }
