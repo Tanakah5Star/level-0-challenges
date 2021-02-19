187ce964f00cbd50cee7986f2f6b48a2268f32c3
tanakah <fivestarlks@gmail.com>
2021-02-19 14:55:03 +0200

    Updated Answer to Task 0.3

---
+++ Task0.3.js
@@ -0,0 +1,6 @@
+function hello(str){
+    //String arguement value concatenated with other string values to create a sentence and get displayed.
+    console.log("Hello " + str +"!");
+
+}
+hello("Tanakah");
