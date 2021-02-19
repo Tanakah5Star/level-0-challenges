e0e660b1fc07cc7afb5049d93cf5e2bf8e928f1e
tanakah <fivestarlks@gmail.com>
2021-02-19 14:59:59 +0200

    Updated Answer to Task 0.8

---
+++ Task0.8.js
@@ -0,0 +1,12 @@
+function convertToHoursMinutes(number){
+    var display= " ";
+    var hours = Math.floor(number/60);//Uses Math.floor method to determin how many hours are in the number.
+    var minutes  = number % 60;//Uses the Modulus operator to get minutes remaining on the number.
+    //Making use of Ternary operators to control singulars and plurals for hours and minutes.
+    if(hours != 0){display += hours +" "+((hours==1)?"hour":"hours")+", ";}
+    if(minutes != 0){display += minutes +" "+((minutes==1)?"minute.":"minutes.");}
+
+    return display;
+    
+}
+convertToHoursMinutes(70);
