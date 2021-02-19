61d78962212fa9bfb2534d13e07822c4f42928a1
Tanakah5Star <77019624+Tanakah5Star@users.noreply.github.com>
2021-02-19 14:30:27 +0200

    Delete Task0,4.js

--- Task0,4.js
+++
@@ -1,6 +0,0 @@
-function evenOrOdd(number){
-    //Ternary Operator used to check if a number is even or odd.
-     const output = (number % 2 == 0) ? "even" : "odd";
-        console.log(output);
-}
-evenOrOdd(91);
