b40cf77ade6c08e210dae499b22fadce1a6348eb
Tanakah5Star <77019624+Tanakah5Star@users.noreply.github.com>
2021-03-02 16:05:49 +0200

    Update and rename Task0.4.js to Task 0.4.js
    Updated  Task 0.4 by removing the unnecessary comments.

--- Task 0.4.js
+++ Task 0.4.js
@@ -1,5 +1,5 @@
 function evenOrOdd(number){
-    //Ternary Operator used to check if a number is even or odd.
+   
      const output = (number % 2 == 0) ? "even" : "odd";
         console.log(output);
 }
