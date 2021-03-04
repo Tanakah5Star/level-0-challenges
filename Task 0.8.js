cabae4dec0c70909cc3989b25da381d6ba6a2761
Tanakah5Star <77019624+Tanakah5Star@users.noreply.github.com>
2021-03-04 13:36:38 +0200

    Update and rename Task0.8.js to Task 0.8.js
    Updated Task 1.8 by removing unnecessary comments and fixing the code to print out the result.

--- Task 0.8.js
+++ Task 0.8.js
@@ -1,8 +1,8 @@
 function convertToHoursMinutes(number){
-    var display= " ";
-    var hours = Math.floor(number/60);//Uses Math.floor method to determin how many hours are in the number.
-    var minutes  = number % 60;//Uses the Modulus operator to get minutes remaining on the number.
-   
+    var display= "";
+    var hours = Math.floor(number/60);
+    var minutes  = number % 60;
+    
        if(hours != 0){
           display += hours +" "+((hours==1)?"hour":"hours")+", ";
        }
@@ -11,4 +11,4 @@ function convertToHoursMinutes(number){
             }
             return display;
  }
-convertToHoursMinutes(70);
+ console.log(convertToHoursMinutes(70);
