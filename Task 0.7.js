5f9be2132b6ab8fe296e9c588a6bab824baa3171
Tanakah5Star <77019624+Tanakah5Star@users.noreply.github.com>
2021-03-04 13:00:34 +0200

    Update and rename Task0.7.js to Task 0.7.js
    Updated Task 1.7 by removing the unnecessary comments and fixing code to display the outputs of the two functions.

--- Task 0.7.js
+++ Task 0.7.js
@@ -1,12 +1,9 @@
-//Function that changes celsius to fahreinheit degrees.
 function celsiusToFahreinheit(celsiusDegrees){
     return fahreinheitDegrees = celsiusDegrees * 1.8 + 32;
 }
-celsiusToFahreinheit(10);
+console.log(celsiusToFahreinheit(10));
 
-//Function that changes fahreinheit to celsius degress.
 function fahreinheitToCelsius(fahreinheitDegrees){
     return celciusDegrees = (fahreinheitDegrees - 32) * (5/9);
 }
-
-fahreinheitToCelsius(50);
+console.log(fahreinheitToCelsius(50));
