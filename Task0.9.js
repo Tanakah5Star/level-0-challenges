5f0966d8812d0f8d719983beff9aad173dbb80ef
Tanakah5Star <77019624+Tanakah5Star@users.noreply.github.com>
2021-04-20 07:03:27 +0200

    Update Task0.9.js
    Updated answer to Task 0.9 by removing unnecessary comments and also fixing the code to remove unwanted spaces in the output.

--- Task0.9.js
+++ Task0.9.js
@@ -1,8 +1,5 @@
-var vowels= "aeiou";
 function printVowels(anyString){
-//Check for vowels, if not a vowel replace with nothing then print vowels.
-   console.log(anyString.replace(/[^aeiou]/ig, " "));
+
+   console.log(anyString.replace(/[^aeiou]/ig, ""));
 }
 printVowels("TKSaaeiO");
-
-   
