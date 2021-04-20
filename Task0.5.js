750a197779598353cb3b65d33e458e3670e0a07a
Tanakah5Star <77019624+Tanakah5Star@users.noreply.github.com>
2021-04-20 06:40:00 +0200

    Update Task0.5.js
    Updated answer to Task 0.5 by removing unnecessary comments, removing duplicate variable declarations and also renaming a variable to a meaningful name.

--- Task0.5.js
+++ Task0.5.js
@@ -1,11 +1,8 @@
-var side1,side2,side3;
-
 function calcTriangleArea(side1,side2,side3){
-//Using Heron's formula to calculate area of triangle using 3 sides
-    const S = (side1 + side2 + side3)/2;
-    const Area = Math.sqrt(S*(S-side1)*(S-side2)*(S-side3));
 
-    return (Area);
-    
+    const Semiperimeter = (side1 + side2 + side3)/2;
+    const Area = Math.sqrt(Semiperimeter*(Semiperimeter-side1)*(Semiperimeter-side2)*(Semiperimeter-side3));
+
+    return (Area);  
 }
-calcTriangleArea(5,3,4);
+console.log(calcTriangleArea(5,3,4));
