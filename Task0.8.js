d0d422b522d46d73e05776ae76d6905669db19d4
Tanakah5Star <77019624+Tanakah5Star@users.noreply.github.com>
2021-07-29 08:36:27 +0200

    Update Task0.8.js
    made use of let instead of var, formatted the code using prettier and also removed the comma in the case that there are no minutes 0n the output.

--- Task0.8.js
+++ Task0.8.js
@@ -1,11 +1,17 @@
-function convertToHoursMinutes(number){
-    var display= " ";
-    var hours = Math.floor(number/60);
-    var minutes  = number % 60;
-    
-    if(hours != 0){display += hours +" "+((hours==1)?"hour":"hours")+", ";}
-    if(minutes != 0){display += minutes +" "+((minutes==1)?"minute.":"minutes.");}
+function convertToHoursMinutes(number) {
+  let display = " ";
+  let hours = Math.floor(number / 60);
+  let minutes = number % 60;
 
-    return display;
+  if (hours != 0 && minutes != 0) {
+    display += hours + " " + (hours == 1 ? "hour" : "hours") + ", ";
+  }
+  if (hours != 0 && minutes == 0) {
+    display += hours + " " + (hours == 1 ? "hour." : "hours.");
+  }
+  if (minutes != 0) {
+    display += minutes + " " + (minutes == 1 ? "minute." : "minutes.");
+  }
+  return display;
 }
-console.log(convertToHoursMinutes(121));
+console.log(convertToHoursMinutes(120));
