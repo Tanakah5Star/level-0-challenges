c9e8dbc71bdc8afa0e046892eceb26183e6aecd6
Tanakah5Star <77019624+Tanakah5Star@users.noreply.github.com>
2021-07-29 08:09:35 +0200

    Update Task0.4.js
    Formatted the code with prettier

--- Task0.4.js
+++ Task0.4.js
@@ -1,5 +1,5 @@
-function evenOrOdd(number){
-     const output = (number % 2 == 0) ? "even" : "odd";
-        console.log(output);
+function evenOrOdd(number) {
+  const output = number % 2 == 0 ? "even" : "odd";
+  console.log(output);
 }
 evenOrOdd(91);
