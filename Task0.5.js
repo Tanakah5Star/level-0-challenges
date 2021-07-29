f131b90a01ad2a60ad61b0822478a8e3271fddcd
Tanakah5Star <77019624+Tanakah5Star@users.noreply.github.com>
2021-07-29 08:10:48 +0200

    Update Task0.5.js
    Used Prettier to format the code.

--- Task0.5.js
+++ Task0.5.js
@@ -1,8 +1,12 @@
-function calcTriangleArea(side1,side2,side3){
+function calcTriangleArea(side1, side2, side3) {
+  const Semiperimeter = (side1 + side2 + side3) / 2;
+  const Area = Math.sqrt(
+    Semiperimeter *
+      (Semiperimeter - side1) *
+      (Semiperimeter - side2) *
+      (Semiperimeter - side3)
+  );
 
-    const Semiperimeter = (side1 + side2 + side3)/2;
-    const Area = Math.sqrt(Semiperimeter*(Semiperimeter-side1)*(Semiperimeter-side2)*(Semiperimeter-side3));
-
-    return (Area);  
+  return Area;
 }
-console.log(calcTriangleArea(5,3,4));
+console.log(calcTriangleArea(5, 3, 4));
