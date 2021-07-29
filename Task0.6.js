de6458b4f47682cf9f9764100de7c7024d518ba1
Tanakah5Star <77019624+Tanakah5Star@users.noreply.github.com>
2021-07-29 08:12:09 +0200

    Update Task0.6.js
    Used Prettier to format the code.

--- Task0.6.js
+++ Task0.6.js
@@ -1,10 +1,9 @@
-function checkMaximumNumber(number1, number2, number3){
-
-   if ( number1 > number2 && number1 > number3){
-         return number1;
-   }else if (number2 > number3){
-         return  number2;
-         }
-         return number3;
+function checkMaximumNumber(number1, number2, number3) {
+  if (number1 > number2 && number1 > number3) {
+    return number1;
+  } else if (number2 > number3) {
+    return number2;
+  }
+  return number3;
 }
-console.log(checkMaximumNumber(78,2,9));
+console.log(checkMaximumNumber(78, 2, 9));
