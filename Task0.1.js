97904e361a171cdaaec85714124996bf39e337f4
Tanakah5Star <77019624+Tanakah5Star@users.noreply.github.com>
2021-07-29 07:58:56 +0200

    Update Task0.1.js
    made use of let instead of var and also made use of prettier to format the code

--- Task0.1.js
+++ Task0.1.js
@@ -1,10 +1,10 @@
-var x = 0;
-var y = 1;
+let x = 0;
+let y = 1;
 
-  console.log(x);
-  console.log(y);
-  
-    x += 3;
-    y += x;
-  console.log(x);
-  console.log(y);
+console.log(x);
+console.log(y);
+
+x += 3;
+y += x;
+console.log(x);
+console.log(y);
