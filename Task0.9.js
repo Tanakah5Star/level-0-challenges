95ca62ddf5c3f25e2bf5c83ed1edfa17561c2e37
Tanakah5Star <77019624+Tanakah5Star@users.noreply.github.com>
2021-08-12 13:00:40 +0200

    Update Task0.9.js
    made changes to code to display and not return.

--- Task0.9.js
+++ Task0.9.js
@@ -1,6 +1,6 @@
 function printVowels(anyString) {
   let vowels = "";
   vowels = anyString.replace(/[^aeiou]/gi, "").split("");
-  return "Vowels: " + vowels;
+  console.log("Vowels: " + vowels);
 }
-console.log(printVowels("House"));
+printVowels("House");
